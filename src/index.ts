20369e964b525e0ec6fe1ba7f0ea46f0cac0243e
DaniDsg11 <Dani_Dsg11@outlook.com>
2024-05-21 14:09:43 +0200

    Add and See Resources

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import * as net from 'net';
 import * as fs from 'fs';
 
+let resources: { [id: string]: any } = {};
+
+const generateId = () => '_' + Math.random().toString(36).substr(2, 9);
+
 // Create TCP server
 const server = net.createServer((socket) => {
     // Handle incoming data
     socket.on('data', (data) => {
         const request = data.toString();
         const [method, path, ...rest] = request.split(' ');
-
+        console.log('Request:', method, path);
+        console.log('Rest:', rest);
+        const rawBody = rest.join(' ');
+        const startIndex = rawBody.indexOf('{');
+        const endIndex = rawBody.lastIndexOf('}');
+        
+        // Extract the JSON body substring
+        const jsonBody = rawBody.substring(startIndex, endIndex + 1);
+        console.log('JSON body:', jsonBody);
         // Set response headers
         const headers = [
             'Content-Type: application/json',
@@ -45,6 +57,45 @@ const server = net.createServer((socket) => {
                     socket.write(`HTTP/1.1 ${response.statusCode}\n${headers.join('\n')}${response.body}`);
                 }
                 break;
+            case '/resources':
+                if (method === 'GET') {
+                    // Return list of resources
+                    const resourceList = Object.keys(resources).map(id => ({ id, data: resources[id] }));
+                    const response = {
+                        statusCode: 200,
+                        body: JSON.stringify(resourceList)
+                    };
+                    socket.write(`HTTP/1.1 ${response.statusCode}\n${headers.join('\n')}\n\n${response.body}`);
+                    socket.end();
+                } else if (method === 'POST') {
+                    console.log('POST');
+                    
+                    try {
+                        const id = generateId();
+                        const parsedJson = JSON.parse(jsonBody);
+                        resources[id] = parsedJson;
+                        const response = {
+                            statusCode: 201,
+                            body: JSON.stringify({ id, data: parsedJson })
+                        };
+                        socket.write(`HTTP/1.1 ${response.statusCode}\n${headers.join('\n')}\n\n${response.body}`);
+                    } catch (error) {
+                        const response = {
+                            statusCode: 400,
+                            body: JSON.stringify({ error: 'Bad Request' })
+                        };
+                        socket.write(`HTTP/1.1 ${response.statusCode}\n${headers.join('\n')}${response.body}`);
+                    }
+                    socket.end();
+                } else {
+                    const response = {
+                        statusCode: 405,
+                        body: JSON.stringify({ error: 'Method Not Allowed' })
+                    };
+                    socket.write(`HTTP/1.1 ${response.statusCode}\n${headers.join('\n')}${response.body}`);
+                    socket.end();
+                }
+                break;
             default:
                 // Return the appropriate error codes if the endpoints are not invoked correctly
                 const response = {
@@ -60,7 +111,7 @@ const server = net.createServer((socket) => {
 });
 
 // Set server port
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 // Start server
 server.listen(PORT, () => {
